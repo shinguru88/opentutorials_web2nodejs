28a4d126390f91deda63c4c0edecee1d66f5e7d7
Shin, Hyun-Chool <shinguru@gmail.com>
2018-12-31 17:51:53 +0900

    update website item list automatically by using readdir function

--- main.js
+++ main.js
@@ -1,42 +1,84 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+
 var app = http.createServer(function (request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
     var pathname = url.parse(_url, true).pathname;
-
     if (pathname === '/') {
-        fs.readFile(`data/${title}`, 'utf-8', function (err, data) {
-            var description = data;
-            var template = `
+        if (queryData.id === undefined) {
+
+            fs.readdir('./data', function (error, filelist) {
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = '<ul>';
+                var i = 0;
+                while (i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                    i = i + 1;
+                }
+                list = list + '</ul>';
+                var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+                response.writeHead(200);
+                response.end(template);
+            })
+
+
+
+        } else {
+            fs.readdir('./data', function (error, filelist) {
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = '<ul>';
+                var i = 0;
+                while (i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                    i = i + 1;
+                }
+                list = list + '</ul>';
+                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                    var title = queryData.id;
+                    var template = `
             <!doctype html>
             <html>
-                <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1><a href="/">WEB</a></h1>
-                    <ol>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                    </ol>
-            
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                </body>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
             </html>
             `;
-            response.writeHead(200);
-            response.end(template);
-        });
+                    response.writeHead(200);
+                    response.end(template);
+                });
+            });
+        }
     } else {
-            response.writeHead(404);
-            response.end('Not found');
+        response.writeHead(404);
+        response.end('Not found');
     }
+
+
 
 });
 app.listen(3000);
---
+++ nodejs/readdir.js
@@ -0,0 +1,5 @@
+var testFolder = './data';
+var fs = require('fs');
+fs.readdir(testFolder, function(error, fileList){
+    console.log(fileList);
+});
