b0cadae75f07b8652c4b24de814a3072a6fafe9e
Shin, Hyun-Chool <shinguru@gmail.com>
2018-12-31 15:15:57 +0900

    javascript 기본 syntax

---
+++ syntax/template.js
@@ -0,0 +1,9 @@
+var name = 'shinguru'
+var letter = 'Dear '+name+'\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi '+name+' ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse shinguru cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. '+name;
+console.log(letter);
+
+var a = '1';
+
+var letter = `Dear ${name}\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ${name} ut aliquip ex ea commodo consequat. ${1+1} Duis aute irure dolor in reprehenderit in voluptate velit esse shinguru cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ${name}`;
+
+console.log(letter);
---
+++ syntax/variable2.js
@@ -0,0 +1,3 @@
+var name = 'shinguru'
+var letter = 'Dear '+name+' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi '+name+' ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse shinguru cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. '+name;
+console.log(letter);
