cc5855fdb725530701eefb79be0d6288476f882c
Shin, Hyun-Chool <shinguru@gmail.com>
2018-12-31 17:14:31 +0900

    Add 'Not found' support

--- main.js
+++ main.js
@@ -4,43 +4,39 @@ var url = require('url');
 var app = http.createServer(function (request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    console.log(queryData.id);
-
     var title = queryData.id;
+    var pathname = url.parse(_url, true).pathname;
 
-    if (_url == '/') {
-        title = 'Welcome';
-    }
-    if (_url == '/favicon.ico') {
-        return response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-
-    fs.readFile(`data/${title}`, 'utf-8', function (err, data) {
-        var description = data;
-        var template = `
-        <!doctype html>
-        <html>
-            <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                <ol>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                </ol>
-        
-                <h2>${title}</h2>
-                <p>${description}</p>
-            </body>
-        </html>
+    if (pathname === '/') {
+        fs.readFile(`data/${title}`, 'utf-8', function (err, data) {
+            var description = data;
+            var template = `
+            <!doctype html>
+            <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ol>
+            
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+            </html>
             `;
-        response.end(template);
-    });
+            response.writeHead(200);
+            response.end(template);
+        });
+    } else {
+            response.writeHead(404);
+            response.end('Not found');
+    }
+
 });
 app.listen(3000);
